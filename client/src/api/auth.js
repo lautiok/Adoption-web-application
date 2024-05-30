799fda8cf3b7d0233f8e0de6f33ccd9361fcbec5
Nahuel Guerra <nahuelguerra56b@gmail.com>
2024-05-30 19:31:30 -0300

    api-autch

--- client/src/api/auth.js
+++ client/src/api/auth.js
@@ -1,8 +1,8 @@
 import axios from "./axios";
 
 
-export const registerRequest = user => axios.post(`session/register`, user)
+export const registerRequest = user => axios.post(`/session/register`, user)
 
-export const loginRequest = user => axios.post(`session/login`, user)
+export const loginRequest = user => axios.post(`/session/login`, user)
 
-export const verifyRequest = () => axios.get(`session/verify`)
+export const verifyRequest = () => axios.get(`/session/verify`)
