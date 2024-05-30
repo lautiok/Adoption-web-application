e31cc391e43a7237b6b16a59b9ac4b34942f20cf
Nahuel Guerra <nahuelguerra56b@gmail.com>
2024-05-30 17:13:20 -0300

    trubb

--- backend/src/dao/mongo/database.js
+++ backend/src/dao/mongo/database.js
@@ -3,7 +3,7 @@ import config from "../../config/config.js"
 
 export const connect = async () => {
     try {
-        await mongoose.connect(config.mongo,)
+        await mongoose.connect(config.mongo, { useNewUrlParser: true, useUnifiedTopology: true })
         console.log('Database connected')
     } catch (error) {
         console.log(error)
