90030b5a39f667b11ee3d9b0f70461f011726ebf
Nahuel Guerra <nahuelguerra56b@gmail.com>
2024-05-31 18:25:35 -0300

    act-01

--- client/src/components/Filter.jsx
+++ client/src/components/Filter.jsx
@@ -20,47 +20,50 @@ export const Filter = ({ onFilter }) => {
   return (
     <>
       <div className="filter-container">
-        <div className="selector">
-          <h3>Tipo</h3>
-          <Select 
-          options={optionsType} 
-             className="select"
-              value={searchTerm}
-              onChange={setSearchTerm}
+  <div className="selector">
+  <div className="tipo">
+    <h3>Tipo</h3>
+    <Select 
+      placeholder="Todos los tipos"
+      options={optionsType} 
+      className="select"
+      value={searchTerm}
+      onChange={setSearchTerm}
+    />
+  </div>
+    <div className="age">
+      <h3>Edad</h3>
+      <Select
+        placeholder="Todas las edades"
+        options={optionsAge}
+        value={selectedAge}
+        onChange={setSelectedAge}
+        className="select"
+      />
+    </div>
+    <div className="gender">
+      <h3>Género</h3>
+      <Select
+        placeholder="Todos los generos"
+        options={optionsGender}
+        value={selectedGender}
+        onChange={setSelectedGender}
+        className="select"
+      />
+    </div>
+  </div>
+  <button className="filter-button-search" onClick={handleFilter}>
+    Filtrar
+  </button>
+</div>
+<div className="filtrado">
+  <p>Filtrado por:</p>
+  {selectedAge && <p className="select-option">{selectedAge.label}</p>}
+  {selectedGender && <p className="select-option">{selectedGender.label}</p>}
+  {searchTerm && <p className="select-option">{searchTerm.label}</p>}
+</div>
 
-             />
-        </div>
-        <div className="selector">
-          
-          <div className="age">
-            <h3>Edad</h3>
-            <Select
-              options={optionsAge}
-              value={selectedAge}
-              onChange={setSelectedAge}
-              className="select"
-            />
-          </div>
-          <div className="gender">
-            <h3>Género</h3>
-            <Select
-              options={optionsGender}
-              value={selectedGender}
-              onChange={setSelectedGender}
-              className="select"
-            />
-          </div>
-        </div>
-        <button className="filter-button-search" onClick={handleFilter}>
-          Filtrar
-        </button>
-      </div>
-      <div className="filtrado">
-        <p>Filtrado por:</p>
-        {selectedAge && <p className="select-option">{selectedAge.label}</p>}
-        {selectedGender && <p className="select-option">{selectedGender.label}</p>}
-        {searchTerm && <p className="select-option">{searchTerm.label}</p>}
-      </div>
+
     </>
   );
 };
