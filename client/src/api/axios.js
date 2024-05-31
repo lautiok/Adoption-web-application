dfb834d7d83448a359c30df75280e002c18b48aa
Nahuel Guerra <nahuelguerra56b@gmail.com>
2024-05-31 13:54:56 -0300

    headers

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -5,6 +5,15 @@ const url = import.meta.env.VITE_BASE_URL;
 const instance = axios.create({
     baseURL: url,
     withCredentials: true,
+    headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
+        "Access-Control-Allow-Headers": "Origin, Content-Type, X-Auth-Token",
+        "X-Requested-With": "XMLHttpRequest",
+        "Access-Control-Allow-Credentials": true
+    },
 });
 
 export default instance
